683569d139925d25267a0f720811e501decfcd31
Matteo Mancini <ingmatteomancini@gmail.com>
2019-09-11 11:36:53 -0400

    Added proper workflow-specific parameters to mrtrix-tckgen.

--- trampolino/workflows/mrtrix_tckgen.py
+++ trampolino/workflows/mrtrix_tckgen.py
@@ -1,20 +1,31 @@
 from nipype.interfaces import utility as util
 from nipype.pipeline import engine as pe
 from .interfaces import mrtrix3 as mrtrix3
+import os.path
 
 
 def create_pipeline(name="tckgen", opt=""):
+
+    parameters = {'nos': 5000,
+                  'include': None}
 
     inputnode = pe.Node(
         interface=util.IdentityInterface(fields=["odf", "seed", "angle", "algorithm"]),
         name="inputnode")
 
-    tckgen = pe.Node(mrtrix3.Tractography(), name='track')
     if opt is not None:
         opt_list = opt.split(',')
-        tckgen.inputs.select = int(opt_list[0])
-        if len(opt_list) > 1:
-            tckgen.inputs.roi_incl = opt_list[1]
+        for o in opt_list:
+            try:
+                [key, value] = o.split(':')
+                parameters[key] = value
+            except ValueError:
+                print(o+': irregular format, skipping')
+
+    tckgen = pe.Node(mrtrix3.Tractography(), name='track')
+    tckgen.inputs.select = int(parameters['nos'])
+    if parameters['include'] is not None:
+        tckgen.inputs.roi_incl = os.abs.path(parameters['include'])
 
     output_fields = ["tck"]
     outputnode = pe.Node(
