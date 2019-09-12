06cf48dc260d5f4fd1248cd43a388129be1a65d4
Matteo Mancini <ingmatteomancini@gmail.com>
2019-09-12 12:12:24 -0400

    Added minimum length iterable and ensemble-only filtering.

--- trampolino/cli.py
+++ trampolino/cli.py
@@ -86,9 +86,10 @@ def dw_recon(ctx, workflow, in_file, bvec, bval, anat, opt):
 @click.option('--angle', type=str, help='Angular threshold(s).')
 @click.option('--angle_range/--angle_values', default=False,
               help='Select a range of angles or the provided values.')
+@click.option('--min_length', type=str, help='Minimum streamline length(s).')
 @click.option('--opt', type=str, help='Workflow-specific optional arguments.')
 @click.pass_context
-def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, opt):
+def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, min_length, opt):
     """Reconstructs the streamlines.
 
     Available workflows: mrtrix_tckgen"""
@@ -100,9 +101,9 @@ def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, opt):
         sys.exit(1)
     wf_sub = wf_mod.create_pipeline(name='tck', opt=opt)
     param = pe.Node(
-        interface=util.IdentityInterface(fields=["angle", "algorithm"]),
+        interface=util.IdentityInterface(fields=["angle", "algorithm", "min_length"]),
         name="param_node")
-    if angle is not None or algorithm is not None:
+    if angle is not None or algorithm is not None or min_length is not None:
         param.iterables = []
     if angle is not None:
         angle_thres = angle.split(',')
@@ -113,6 +114,10 @@ def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, opt):
     if algorithm is not None:
         algs = algorithm.split(',')
         param.iterables.append(('algorithm', algs))
+    if min_length is not None:
+        mls = min_length.split(',')
+        mls = [int(l) for l in mls if l.isdigit()]
+        param.iterables.append(('min_length', mls))
     wf = ctx.obj['workflow']
     if seed is not None:
         wf_sub.inputs.inputnode.seed = click.format_filename(seed)
@@ -126,6 +131,7 @@ def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, opt):
             wf.connect([(ctx.obj['recon'], wf_sub, [("outputnode.seed", "inputnode.seed")])])
     wf.connect([(param, wf_sub, [("angle", "inputnode.angle")]),
                 (param, wf_sub, [("algorithm", "inputnode.algorithm")]),
+                (param, wf_sub, [("min_length", "inputnode.min_length")]),
                 (wf_sub, ctx.obj['results'], [("outputnode.tck", "@tck")])])
     ctx.obj['track'] = wf_sub
     ctx.obj['param'] = param
@@ -133,7 +139,7 @@ def odf_track(ctx, workflow, odf, seed, algorithm, angle, angle_range, opt):
 
 
 @cli.command('filter')
-@click.argument('workflow', required=True)
+@click.argument('workflow', required=False)
 @click.option('-t', '--tck', type=click.Path(exists=True, resolve_path=True),
               help='Reconstructed streamlines.')
 @click.option('-o', '--odf', type=click.Path(exists=True, resolve_path=True),
@@ -147,29 +153,39 @@ def tck_filter(ctx, workflow, tck, odf, ensemble):
     Available workflows: mrtrix_tcksift"""
 
     try:
-        wf_mod = import_module('.workflows.' + workflow, package='trampolino')
+        if workflow is not None or not ensemble:
+            wf_mod = import_module('.workflows.' + workflow, package='trampolino')
+            wf_sub = wf_mod.create_pipeline(name='tck_post')
     except ImportError as err:
         click.echo(workflow + ' is not a valid workflow.')
         sys.exit(1)
-    wf_sub = wf_mod.create_pipeline(name='tck_post')
+    except TypeError as err:
+        click.echo('No workflow selected (maybe --ensemble is missing?).')
+        sys.exit(1)
     wf = ctx.obj['workflow']
     if 'track' not in ctx.obj:
+        if workflow is None:
+            click.echo('No workflow selected.')
+            sys.exit(1)
         wf_sub.inputs.inputnode.tck = click.format_filename(tck)
         wf_sub.inputs.inputnode.odf = click.format_filename(odf)
         wf.add_nodes([wf_sub])
+        wf.connect([(wf_sub, ctx.obj['results'], [("outputnode.tck_post", "@tck_post")])])
     else:
         wf = ctx.obj['workflow']
-        wf.add_nodes([wf_sub])
         if ensemble:
             tck_merged = pe.JoinNode(interface=TckEdit(), joinsource=ctx.obj['param'],
                                      joinfield=["in_files"], name="merge")
             wf.connect([(ctx.obj['track'], tck_merged, [("outputnode.tck", "in_files")]),
-                        (tck_merged, wf_sub, [("out_file", "inputnode.tck")]),
                         (tck_merged, ctx.obj['results'], [("out_file", "@tck_merged")])])
-        else:
-            wf.connect([(ctx.obj['track'], wf_sub, [("outputnode.tck", "inputnode.tck")])])
-        wf.connect([(ctx.obj['track'], wf_sub, [("inputnode.odf", "inputnode.odf")])])
-    wf.connect([(wf_sub, ctx.obj['results'], [("outputnode.tck_post", "@tck_post")])])
+        if workflow is not None:
+            wf.add_nodes([wf_sub])
+            if ensemble:
+                wf.connect([(tck_merged, wf_sub, [("out_file", "inputnode.tck")])])
+            else:
+                wf.connect([(ctx.obj['track'], wf_sub, [("outputnode.tck", "inputnode.tck")])])
+            wf.connect([(ctx.obj['track'], wf_sub, [("inputnode.odf", "inputnode.odf")]),
+                        (wf_sub, ctx.obj['results'], [("outputnode.tck_post", "@tck_post")])])
     return workflow
 
 
