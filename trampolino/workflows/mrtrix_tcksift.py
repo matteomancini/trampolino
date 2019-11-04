11a25f55699728d39bd08acad9b412b4dedb9a38
Matteo Mancini <ingmatteomancini@gmail.com>
2019-11-04 10:08:59 -0500

    Updated MRtrix filtering workflow.

--- trampolino/workflows/mrtrix_tcksift.py
+++ trampolino/workflows/mrtrix_tcksift.py
@@ -5,11 +5,24 @@ from .interfaces import mrtrix3 as mrtrix3
 
 def create_pipeline(name="tcksift", opt=""):
 
+    parameters = {'term_n': None}
+
     inputnode = pe.Node(
         interface=util.IdentityInterface(fields=["tck", "odf"]),
         name="inputnode")
 
+    if opt is not None:
+        opt_list = opt.split(',')
+        for o in opt_list:
+            try:
+                [key, value] = o.split(':')
+                parameters[key] = value
+            except ValueError:
+                print(o+': irregular format, skipping')
+
     tcksift = pe.Node(mrtrix3.TckSIFT(), name='SIFT')
+    if parameters['term_n'] is not None:
+        tcksift.inputs.term_number = int(parameters['term_n'])
 
     output_fields = ["tck_post"]
     outputnode = pe.Node(
@@ -20,7 +33,7 @@ def create_pipeline(name="tcksift", opt=""):
     workflow.base_output_dir = name
 
     workflow.connect([(inputnode, tcksift, [("tck", "in_file"),
-                                           ("odf", "in_fod")])])
+                                            ("odf", "in_fod")])])
 
     workflow.connect([
         (tcksift, outputnode, [("out_file", "tck_post")])
